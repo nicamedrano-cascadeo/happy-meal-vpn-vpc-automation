eef6d39204f73b7d6e733ac33409088fa6388a3d
Nica <veronica@cascadeo.com>
2021-07-19 11:39:07 +0800

    working model

--- main.tf
+++ main.tf
@@ -18,10 +18,7 @@ data "aws_availability_zones" "available" {
 
 locals {
   max_subnet_length = max(
-    length(var.private_subnets),
-    length(var.elasticache_subnets),
-    length(var.database_subnets),
-    length(var.redshift_subnets),
+    length(var.private_subnets)
   )
   nat_gateway_count = var.single_nat_gateway ? 1 : var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length
 
@@ -72,49 +69,6 @@ resource "aws_vpc_ipv4_cidr_block_association" "this" {
   cidr_block = element(var.secondary_cidr_blocks, count.index)
 }
 
-resource "aws_default_security_group" "this" {
-  count = var.create_vpc && var.manage_default_security_group ? 1 : 0
-
-  vpc_id = aws_vpc.this[0].id
-
-  dynamic "ingress" {
-    for_each = var.default_security_group_ingress
-    content {
-      self             = lookup(ingress.value, "self", null)
-      cidr_blocks      = compact(split(",", lookup(ingress.value, "cidr_blocks", "")))
-      ipv6_cidr_blocks = compact(split(",", lookup(ingress.value, "ipv6_cidr_blocks", "")))
-      prefix_list_ids  = compact(split(",", lookup(ingress.value, "prefix_list_ids", "")))
-      security_groups  = compact(split(",", lookup(ingress.value, "security_groups", "")))
-      description      = lookup(ingress.value, "description", null)
-      from_port        = lookup(ingress.value, "from_port", 0)
-      to_port          = lookup(ingress.value, "to_port", 0)
-      protocol         = lookup(ingress.value, "protocol", "-1")
-    }
-  }
-
-  dynamic "egress" {
-    for_each = var.default_security_group_egress
-    content {
-      self             = lookup(egress.value, "self", null)
-      cidr_blocks      = compact(split(",", lookup(egress.value, "cidr_blocks", "")))
-      ipv6_cidr_blocks = compact(split(",", lookup(egress.value, "ipv6_cidr_blocks", "")))
-      prefix_list_ids  = compact(split(",", lookup(egress.value, "prefix_list_ids", "")))
-      security_groups  = compact(split(",", lookup(egress.value, "security_groups", "")))
-      description      = lookup(egress.value, "description", null)
-      from_port        = lookup(egress.value, "from_port", 0)
-      to_port          = lookup(egress.value, "to_port", 0)
-      protocol         = lookup(egress.value, "protocol", "-1")
-    }
-  }
-
-  tags = merge(
-    {
-      "Name" = format("%s", var.default_security_group_name)
-    },
-    var.tags,
-    var.default_security_group_tags,
-  )
-}
 
 
 ###################
